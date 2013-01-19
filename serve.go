1be6d1f07e4a05eb34d0e40f2cf0e46b98281f19
Sasha Crofter <sasha@crofter.org>
2013-01-18 20:59:41 -0500

    Add basic (untested, but compiling) html server
    Once connected to the startup process (flags should be added first,) the
    http server should behave sensibly.

---
+++ serve.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+)
+
+var rootpath string
+
+// Serve starts an http server using net/http which listens on the given
+// interface and port. It serves files using the given path as its root
+// directory. If iface is blank, 0.0.0.0 is assumed by net/http.
+//
+// In order to pass information to the handler functions, it sets the
+// rootpath variable. This means that invoking this function a second
+// time will impair the behavior of the first call.
+//
+// This function will block until the http server encounters an error or
+// otherwise stops.
+func Serve(root, iface string, port int) (err error) {
+	// Root the path, and clean it if necessary.
+
+	// 18/01/2013 It might make sense to move this to a helper routine
+	// or further up in the stack.
+	if !path.IsAbs(root) {
+		var wd string
+		wd, err = os.Getwd()
+		if err != nil {
+			return
+		}
+		root = path.Join(wd, root)
+	} else {
+		root = path.Clean(root)
+	}
+	l.Printf("Starting http server %s:%d\nRoot path %q", iface, port, root)
+	rootpath = root
+
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(iface+":"+strconv.Itoa(port), nil)
+	return
+}
+
+// Handle is the primary (read: only) http handler. It uses the variable
+// rootpath to determine where to serve content from.
+func handle(w http.ResponseWriter, req *http.Request) {
+	// 18/01/2013 I hope this doesn't turn into a god function.
+	w.Write([]byte("Hello, Forest."))
+}

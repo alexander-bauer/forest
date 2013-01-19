e4a3569ca413bb0c13ebf9352f5f8eb8dd1f75fa
Sasha Crofter <sasha@crofter.org>
2013-01-18 22:54:25 -0500

    Add server flags (--bind and --port)

--- forest.go
+++ forest.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 )
@@ -12,10 +13,26 @@ var (
 	l = log.New(os.Stdout, "", log.Ldate|log.Ltime)
 )
 
-func main() {
-	logInfo(l)
-}
+// Default values for flags
+const (
+	Bind = "0.0.0.0" // Interface to bind to
+	Port = 8866      // Port to listen on
+)
 
-func logInfo(l *log.Logger) {
-	l.Printf(`Forest v%s`, Version+minversion)
+// Flag declarations
+var (
+	fRoot = flag.String("root", "", "project root")
+
+	fBind = flag.String("bind", Bind, "interface to bind to")
+	fPort = flag.Int("port", Port, "port to listen on")
+)
+
+func main() {
+	log.Printf("Forest %s%s\n", Version, minversion)
+	flag.Parse()
+
+	err := Serve(*fRoot, *fBind, *fPort)
+	if err != nil {
+		l.Printf("Forest error: %s", err)
+	}
 }
